0450aa093d61fea2894454ab197b7a835de5876b
vsh123 <32254689+vsh123@users.noreply.github.com>
2019-10-21 14:24:59 +0900

    initial commit (#1)

---
+++ src/main/java/woowacrew/WoowaCrewApplication.java
@@ -0,0 +1,11 @@
+package woowacrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowaCrewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WoowaCrewApplication.class, args);
+    }
+}
