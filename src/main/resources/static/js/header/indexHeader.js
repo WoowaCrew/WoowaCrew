5667d8b969ae91b392b810b2c1fadd06ed2a603a
hyojaekim <hj00686@naver.com>
2019-11-04 18:15:04 +0900

    style: Index 페이지 프론트 수정
    - 게이지, 나이, 사이드바 js 추가

--- src/main/resources/static/js/header/indexHeader.js
+++ src/main/resources/static/js/header/indexHeader.js
@@ -1,23 +1,40 @@
-var i = 0;
-var date = Math.ceil(new Date().getMonth() / 12  * 100);
-
 move();
+age();
 
 function move() {
-    if (i == 0) {
-        i = 1;
-        var elem = document.getElementById("myBar");
-        var width = 0;
-        var id = setInterval(frame, 5);
-        function frame() {
-            if (width >= date) {
-                clearInterval(id);
-                i = 0;
-            } else {
-                width++;
-                elem.style.width = width + "%";
-                elem.innerHTML = width  + "%";
-            }
+    var date = Math.ceil(new Date().getMonth() / 12  * 100);
+    var progressBarDiv = document.getElementById("myBar");
+    var width = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if (width >= date) {
+            clearInterval(id);
+        } else {
+            width++;
+            progressBarDiv.style.width = width + "%";
+            progressBarDiv.innerHTML = width  + "%";
         }
+    }
+}
+
+function age() {
+    var ageDiv = document.getElementById("userAge");
+    var age = ageDiv.dataset.age;
+
+    if (age < 25) {
+        ageDiv.innerText = "🐣";
+    } else if (25 <= age && age < 30) {
+        ageDiv.innerText = "🐥";
+    } else if (age >= 30) {
+        ageDiv.innerText = "🐲";
+    }
+}
+
+function toggleNav() {
+    var sidenavDiv = document.getElementById("mySidenav");
+    if (sidenavDiv.style.width === "250px") {
+        sidenavDiv.style.width = "0px";
+    } else {
+        sidenavDiv.style.width = "250px";
     }
 }
