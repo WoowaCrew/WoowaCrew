1f8de8c286ff4aeefaa6ab26442ef3db89c9e71f
SunHo Van <vsh123@naver.com>
2019-11-06 21:15:37 +0900

    feat: GithubConfig 구현
    - yaml파일 읽기를 위한 PropertySourceFactory 구현

---
+++ src/main/java/woowacrew/oauth/GithubConfig.java
@@ -0,0 +1,57 @@
+package woowacrew.oauth;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import woowacrew.utils.YamlPropertySourceFactory;
+
+@Configuration
+@PropertySource(value = "classpath:github.yml", factory = YamlPropertySourceFactory.class)
+@ConfigurationProperties(prefix = "github.client")
+public class GithubConfig {
+    private String clientId;
+    private String clientSecret;
+    private String userAuthorizationUri;
+    private String accessTokenUri;
+    private String userInfoUri;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public String getUserAuthorizationUri() {
+        return userAuthorizationUri;
+    }
+
+    public void setUserAuthorizationUri(String userAuthorizationUri) {
+        this.userAuthorizationUri = userAuthorizationUri;
+    }
+
+    public String getAccessTokenUri() {
+        return accessTokenUri;
+    }
+
+    public void setAccessTokenUri(String accessTokenUri) {
+        this.accessTokenUri = accessTokenUri;
+    }
+
+    public String getUserInfoUri() {
+        return userInfoUri;
+    }
+
+    public void setUserInfoUri(String userInfoUri) {
+        this.userInfoUri = userInfoUri;
+    }
+}
