8e52f1475c7434e943f85e9dfcb12282171365bd
hyojaekim <hj00686@naver.com>
2019-11-16 17:01:39 +0900

    feat: 검색 기능 인터셉터 제외

--- src/main/java/woowacrew/utils/configuration/WebMvcConfig.java
+++ src/main/java/woowacrew/utils/configuration/WebMvcConfig.java
@@ -18,6 +18,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/login", "/css/**", "/image/**", "/js/**", "/oauth/**");
+                .excludePathPatterns("/", "/login", "/css/**", "/image/**", "/js/**", "/oauth/**", "/search?**");
     }
 }
