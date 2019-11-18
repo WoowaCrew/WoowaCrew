fe7d2e6abbd9d053c2ebbf67c93337d195e717d0
SunHo Van <vsh123@naver.com>
2019-11-18 14:04:44 +0900

    feat: Test를 위한 LoginController

--- src/test/java/woowacrew/oauth/controller/TestLoginController.java
+++ src/test/java/woowacrew/oauth/controller/TestLoginController.java
@@ -1,16 +1,24 @@
 package woowacrew.oauth.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import woowacrew.user.domain.UserDto;
+import woowacrew.user.domain.User;
+import woowacrew.user.domain.UserConverter;
+import woowacrew.user.domain.UserRepository;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class TestLoginController {
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/test/login")
     public String testLogin(HttpSession session) {
-        session.setAttribute("user", new UserDto("test", "test"));
+        User user = new User("test", "test");
+        userRepository.save(user);
+        session.setAttribute("user", UserConverter.userToUserDto(user));
         return "index";
     }
 }
