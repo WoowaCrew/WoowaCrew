b4f723003736fa3d51621702061d72f505b9f25e
hyojaekim <hj00686@naver.com>
2019-11-21 15:42:58 +0900

    refactor: indexHeader JS

--- src/main/resources/static/js/header/indexHeader.js
+++ src/main/resources/static/js/header/indexHeader.js
@@ -1,38 +1,44 @@
-move();
-age();
+function indexHeader() {
+    document.getElementById("myProgress").addEventListener("click", move);
 
-function move() {
-    var date = Math.ceil(new Date().getMonth() / 12  * 100);
-    var progressBarDiv = document.getElementById("myBar");
-    var width = 0;
-    var id = setInterval(frame, 5);
-    function frame() {
-        if (width >= date) {
-            clearInterval(id);
-        } else {
-            width++;
-            progressBarDiv.style.width = width + "%";
-            progressBarDiv.innerHTML = width  + "%";
+    function move() {
+        const date = Math.ceil(new Date().getMonth() / 12  * 100);
+        const progressBarDiv = document.getElementById("myBar");
+        let width = 0;
+        const id = setInterval(frame, 5);
+        function frame() {
+            if (width >= date) {
+                clearInterval(id);
+            } else {
+                width++;
+                progressBarDiv.style.width = width + "%";
+                progressBarDiv.innerHTML = width  + "%";
+            }
         }
     }
-}
 
-function age() {
-    var ageDiv = document.getElementById("userAge");
-    var age = ageDiv.dataset.age;
+    function age() {
+        const ageDiv = document.getElementById("userAge");
+        const age = ageDiv.dataset.age;
 
-    if (age <= 25) {
-        ageDiv.innerText = "🐣";
-    } else if (25 < age && age < 29) {
-        ageDiv.innerText = "🐥";
-    } else if (29 == age) {
-        ageDiv.innerText = "🐔";
-    } else {
-        ageDiv.innerText = "🐲";
+        if (age <= 25) {
+            ageDiv.innerText = "🐣";
+        } else if (25 < age && age < 29) {
+            ageDiv.innerText = "🐥";
+        } else if (29 === age) {
+            ageDiv.innerText = "🐔";
+        } else {
+            ageDiv.innerText = "🐲";
+        }
     }
+
+    function search() {
+        const searchContent = document.getElementsByClassName("search-form")[0].value;
+        location.href="https://www.google.com/search?q=" + searchContent;
+    }
+
+    move();
+    age();
 }
 
-function search() {
-    var searchContent = document.getElementsByClassName("search-form")[0].value;
-    location.href="https://www.google.com/search?q=" + searchContent;
-}
+indexHeader();
