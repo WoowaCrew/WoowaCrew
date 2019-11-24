e6c87213b2b2601fa0e1ebf187350346084122bc
hyojaekim <hj00686@naver.com>
2019-11-24 19:02:43 +0900

    refactor: 조회수 증가 GET 요청 -> POST 요청 변경

--- src/main/resources/static/js/keywordRank.js
+++ src/main/resources/static/js/keywordRank.js
@@ -1,10 +1,14 @@
 document.getElementById("keyword-rank").addEventListener("click", function(event) {
-    const content = event.target.closest(".keyword").getElementsByClassName("keyword-content")[0].textContent;
+    const target = event.target;
+    if (target.closest("tr").className === "keyword") {
+        const keywordArea = target.closest("tr");
+        const keywordId = keywordArea.id;
 
-    fetch("/api/search?content=" + content, {
-        method: 'GET'
-    }).then(function (response) {
-        const url = response.headers.get("Location");
-        window.location.replace(url);
-    });
+        fetch("/search/" + keywordId, {
+            method: "POST"
+        }).then(function (response) {
+            const url = response.headers.get("Location");
+            window.location.replace(url);
+        });
+    }
 });
