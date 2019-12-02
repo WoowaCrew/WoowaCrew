563a840406d54668e41bcad21609b7d52a83b41b
SunHo Van <vsh123@naver.com>
2019-12-03 01:02:59 +0900

    feat: 로그인 페이지

--- src/main/java/woowacrew/utils/configuration/WebMvcConfig.java
+++ src/main/java/woowacrew/utils/configuration/WebMvcConfig.java
@@ -1,9 +1,13 @@
 package woowacrew.utils.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import woowacrew.utils.interceptor.AuthInterceptor;
+import woowacrew.utils.resolver.CustomHandlerMethodArgumentResolver;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -20,5 +24,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/login", "/css/**", "/image/**", "/js/**", "/oauth/**")
                 .excludePathPatterns("/search", "/search/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers
+                .add(new CustomHandlerMethodArgumentResolver());
     }
 }
