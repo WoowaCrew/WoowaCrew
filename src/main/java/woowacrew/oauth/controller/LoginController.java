43e0097b1c0762cf6e541c906907248df7c3c02e
SunHo Van <vsh123@naver.com>
2019-12-03 01:05:49 +0900

    feat: /accessdeny

--- src/main/java/woowacrew/oauth/controller/LoginController.java
+++ src/main/java/woowacrew/oauth/controller/LoginController.java
@@ -1,29 +1,14 @@
 package woowacrew.oauth.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import woowacrew.oauth.Oauth;
-import woowacrew.oauth.service.LoginService;
 import woowacrew.user.domain.User;
 import woowacrew.utils.annotation.AuthenticationUser;
 
 @Controller
 public class LoginController {
-    public static final String USER = "user";
-    public static final String ACCESS_TOKEN = "accessToken";
-
-    private final Oauth oauth;
-    private final LoginService loginService;
-
-    @Autowired
-    public LoginController(Oauth oauth, LoginService loginService) {
-        this.oauth = oauth;
-        this.loginService = loginService;
-    }
-
     @GetMapping("/login")
     public String login() {
         return "login";
@@ -45,5 +30,17 @@ public class LoginController {
     @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public ResponseEntity<User> hello(@AuthenticationUser User user) {
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/accessdeny")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public String accessDeny(@AuthenticationUser User user) {
+        //Todo: User의 Role을 체크해서 그에 맞는 정보를 리턴해 준다.
+        /*Todo:
+           1. 승인 대기 상태
+           2. 회원정보 미입력 상태
+           3. 이 외의 요청은 모두 권한이 없어서 그럼
+         */
+        return "index";
     }
 }
