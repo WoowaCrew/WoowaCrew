f5740ead82eb0c1242efa808dbddb87182864be5
SunHo Van <vsh123@naver.com>
2019-12-03 01:00:38 +0900

    feat: SocialLoginFilter
    - 프로덕션용, 테스트용 두가지 생성

---
+++ src/main/java/woowacrew/security/filter/SocialLoginFilter.java
@@ -0,0 +1,37 @@
+package woowacrew.security.filter;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import woowacrew.security.token.SocialPreAuthorizationToken;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SocialLoginFilter extends AbstractAuthenticationProcessingFilter {
+    public  SocialLoginFilter(String defaultFilterProcessesUrl) {
+        super(defaultFilterProcessesUrl);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        String code = request.getParameter("code");
+
+        return super.getAuthenticationManager().authenticate(new SocialPreAuthorizationToken(code, code));
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+        context.setAuthentication(authResult);
+        SecurityContextHolder.setContext(context);
+
+        response.sendRedirect("/hello");
+    }
+}
