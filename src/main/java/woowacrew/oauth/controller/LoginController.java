5e6e563e841e9d514a8684a75b3c0f11e5e88bed
SunHo Van <vsh123@naver.com>
2019-12-03 17:19:02 +0900

    refactor: 불필요 controller 삭제

--- src/main/java/woowacrew/oauth/controller/LoginController.java
+++ src/main/java/woowacrew/oauth/controller/LoginController.java
@@ -1,10 +1,8 @@
 package woowacrew.oauth.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import woowacrew.user.domain.User;
 import woowacrew.user.domain.UserContext;
 import woowacrew.utils.annotation.AuthenticationUser;
 
@@ -13,24 +11,6 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "login";
-    }
-
-    @GetMapping("/userPage")
-    @PreAuthorize("hasRole('USER')")
-    public String user(@AuthenticationUser User user) {
-        return "userPage";
-    }
-
-    @GetMapping("/admin")
-    @PreAuthorize("hasRole('ADMIN')")
-    public String admin() {
-        return "admin";
-    }
-
-    @GetMapping("/hello")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    public ResponseEntity<UserContext> hello(@AuthenticationUser UserContext userContext) {
-        return ResponseEntity.ok(userContext);
     }
 
     @GetMapping("/accessdeny")
