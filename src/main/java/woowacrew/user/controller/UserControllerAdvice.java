51628b27f3b68da04a8af47d82c58a3635182942
hyojaekim <hj00686@naver.com>
2019-12-05 14:33:02 +0900

    refactor: 불필요한 파라미터 제거

--- src/main/java/woowacrew/user/controller/UserControllerAdvice.java
+++ src/main/java/woowacrew/user/controller/UserControllerAdvice.java
@@ -4,14 +4,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
-import woowacrew.user.domain.UserContext;
 import woowacrew.user.service.exception.InvalidBirthdayException;
-import woowacrew.utils.annotation.AuthenticationUser;
 
 @ControllerAdvice
 public class UserControllerAdvice {
     @ExceptionHandler(InvalidBirthdayException.class)
-    public RedirectView exceptBirthday(InvalidBirthdayException exception, RedirectAttributes redirectAttributes, @AuthenticationUser UserContext userContext) {
+    public RedirectView exceptBirthday(InvalidBirthdayException exception, RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("error", exception.getMessage());
 
         return new RedirectView("/users/form");
