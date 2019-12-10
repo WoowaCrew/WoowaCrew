249dcefeae4bddcb9b219d54ab9ae73d8c9ab376
SunHo Van <vsh123@naver.com>
2019-12-10 21:27:06 +0900

    style: 페이지네이션

--- src/main/resources/static/js/article-list.js
+++ src/main/resources/static/js/article-list.js
@@ -1,15 +1,23 @@
 function articleList() {
   const articleList = document.getElementById('article-list')
   const articleCreateIcon = document.getElementById('article-create-icon')
+  const pageBar = document.getElementById('page-bar')
 
   const origin = window.location.origin
 
   const convertTime = (time) => {
-       return time.split('T')[0]
+    return time.split('T')[0]
   }
 
+  const prevBarForm = '<div class="prev-bar">이전</div>'
+  const nextBarForm = '<div class="next-bar">다음</div>'
+  const pageBarForm = (numberOfPage) =>
+    `<div class="page-number">${numberOfPage}</div>`
+  const selectedPageBarForm = (numberOfPage) =>
+    `<div class="page-number selected-page">${numberOfPage}</div>`
+
   const articleListForm = (article) =>
-      `<div class="article-info">
+    `<div class="article-info">
         <div class="article-info-article-id">
             <div class="article-info-article-id-content" value=${article.id}>
                 ${article.id}
@@ -26,6 +34,37 @@ function articleList() {
         </div>
       </div>`
 
+  fetch(origin + "/api/articles", {
+    method: 'GET'
+  }).then(response => response.json())
+    .then(articleResponse => {
+      renderPageBar(articleResponse.pageNumber, articleResponse.totalPages)
+      articleResponse.articles.forEach(article => {
+        articleList.insertAdjacentHTML("beforeend", articleListForm(article))
+      })
+    })
+    .catch(error => alert('오류가 발생했습니다.'));
+
+  function renderPageBar(pageNumber, totalPages) {
+    pageBar.innerHTML = ''
+
+    const startPageNumber = (pageNumber - 5) > 0 ? pageNumber - 5 : 1
+    if (startPageNumber !== 1) {
+      pageBar.insertAdjacentHTML("beforeend", prevBarForm)
+    }
+    const lastPageNumber = (pageNumber + 5) <= totalPages ? pageNumber + 5 : totalPages
+    for (let numberOfPage = startPageNumber; numberOfPage <= lastPageNumber; numberOfPage++) {
+      if (numberOfPage === pageNumber) {
+        pageBar.insertAdjacentHTML("beforeend", selectedPageBarForm(numberOfPage))
+      } else {
+        pageBar.insertAdjacentHTML("beforeend", pageBarForm(numberOfPage))
+      }
+    }
+    if (lastPageNumber !== totalPages) {
+      pageBar.insertAdjacentHTML("beforeend", nextBarForm)
+    }
+  }
+
   articleList.addEventListener('mouseover', function (e) {
     const node = e.target.parentNode
     if (node.className === 'article-info') {
@@ -41,19 +80,43 @@ function articleList() {
     }
   })
 
-  articleCreateIcon.addEventListener('click', function (e) {
-      window.location.href = origin + '/article/new'
+
+  pageBar.addEventListener('click', function (e) {
+    const node = e.target
+    if (node.className === 'page-number') {
+      const pageNumber = node.innerText
+      getArticle(pageNumber)
+    }
+    if (node.className === 'next-bar') {
+      const pageNumbers = node.parentElement.getElementsByClassName('page-number')
+      const pageNumber = pageNumbers[pageNumbers.length - 1].innerText
+      console.log(pageNumber)
+      getArticle(parseInt(pageNumber) + 1)
+    }
+    if (node.className === 'prev-bar') {
+      const pageNumber = node.parentElement.getElementsByClassName('page-number')[0].innerText
+      getArticle(pageNumber - 1)
+    }
   })
 
-  fetch(origin + "/api/articles", {
-    method: 'GET'
-  }).then(response => response.json())
-      .then(articles => {
-        articles.forEach(article => {
+  function getArticle(pageNumber) {
+    fetch(origin + "/api/articles?page=" + pageNumber, {
+      method: 'GET'
+    }).then(response => response.json())
+      .then(articleResponse => {
+        renderPageBar(articleResponse.pageNumber, articleResponse.totalPages)
+        articleList.innerHTML = ''
+        articleResponse.articles.forEach(article => {
           articleList.insertAdjacentHTML("beforeend", articleListForm(article))
         })
       })
       .catch(error => alert('오류가 발생했습니다.'));
+  }
+
+  articleCreateIcon.addEventListener('click', function (e) {
+    window.location.href = origin + '/article/new'
+  })
+
 }
 
 articleList()
