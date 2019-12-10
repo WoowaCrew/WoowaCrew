b542037c2e03165e4cf8262e993b8eda611b4406
SunHo Van <vsh123@naver.com>
2019-12-10 16:33:50 +0900

    feat: paging 설정 yml파일로 분리

--- src/main/java/woowacrew/utils/configuration/WebMvcConfig.java
+++ src/main/java/woowacrew/utils/configuration/WebMvcConfig.java
@@ -1,8 +1,6 @@
 package woowacrew.utils.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -24,20 +22,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers
                 .add(new CustomHandlerMethodArgumentResolver());
-        resolvers
-                .add(pageableHandlerMethodArgumentResolver());
     }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new UserRoleConverter());
-    }
-
-    @Bean
-    public PageableHandlerMethodArgumentResolver pageableHandlerMethodArgumentResolver() {
-        PageableHandlerMethodArgumentResolver pageResolver = new PageableHandlerMethodArgumentResolver();
-        pageResolver.setOneIndexedParameters(true);
-
-        return pageResolver;
     }
 }
