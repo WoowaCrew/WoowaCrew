fcfddf1165b2a4bc985a4e19df326fc5ef6a93fb
SunHo Van <vsh123@naver.com>
2019-12-11 15:43:45 +0900

    refactor: article-list.js

--- src/main/resources/static/js/article-list.js
+++ src/main/resources/static/js/article-list.js
@@ -1,122 +1,74 @@
-function articleList() {
-  const articleList = document.getElementById('article-list')
-  const articleCreateIcon = document.getElementById('article-create-icon')
-  const pageBar = document.getElementById('page-bar')
+"use strict";
 
-  const origin = window.location.origin
+const ArticleListApp = (() => {
+  const BASE_URL = window.location.origin
 
-  const convertTime = (time) => {
-    return time.split('T')[0]
-  }
+  class ArticleService {
+    async showArticles(numberOfPage) {
+      const articleList = document.getElementById('article-list')
 
-  const prevBarForm = '<div class="prev-bar">이전</div>'
-  const nextBarForm = '<div class="next-bar">다음</div>'
-  const pageBarForm = (numberOfPage) =>
-    `<div class="page-number">${numberOfPage}</div>`
-  const selectedPageBarForm = (numberOfPage) =>
-    `<div class="page-number selected-page">${numberOfPage}</div>`
-
-  const articleListForm = (article) =>
-    `<div class="article-info">
-        <div class="article-info-article-id">
-            <div class="article-info-article-id-content" value=${article.id}>
-                ${article.id}
-            </div>
-        </div>
-        <div class="article-info-title">${article.title}</div>
-        <div class="article-info-created-date">${convertTime(article.createdDate)}</div>
-        <div class="article-info-userInfo">${article.userResponseDto.nickname}</div>
-        <div class="article-info-views">
-            <div class="article-info-views-content">
-                <i class="fa fa-eye"></i>
-                1
-            </div>
-        </div>
-      </div>`
-
-  fetch(origin + "/api/articles", {
-    method: 'GET'
-  }).then(response => response.json())
-    .then(articleResponse => {
-      renderPageBar(articleResponse.pageNumber, articleResponse.totalPages)
-      articleResponse.articles.forEach(article => {
-        articleList.insertAdjacentHTML("beforeend", articleListForm(article))
-      })
-    })
-    .catch(error => alert('오류가 발생했습니다.'));
-
-  function renderPageBar(pageNumber, totalPages) {
-    pageBar.innerHTML = ''
-
-    const startPageNumber = (pageNumber - 5) > 0 ? pageNumber - 5 : 1
-    if (startPageNumber !== 1) {
-      pageBar.insertAdjacentHTML("beforeend", prevBarForm)
+      fetch(BASE_URL + "/api/articles?page=" + numberOfPage, {
+        method: 'GET'
+      }).then(response => response.json())
+        .then(articleResponse => {
+          this.renderPageBar(articleResponse.pageNumber, articleResponse.totalPages)
+          articleList.innerHTML = ''
+          articleResponse.articles.forEach(article => {
+            articleList.insertAdjacentHTML("beforeend", ArticleTemplates.articleListTemplate(article))
+          })
+        })
+        .catch(error => alert('오류가 발생했습니다.'));
     }
-    const lastPageNumber = (pageNumber + 5) <= totalPages ? pageNumber + 5 : totalPages
-    for (let numberOfPage = startPageNumber; numberOfPage <= lastPageNumber; numberOfPage++) {
-      if (numberOfPage === pageNumber) {
-        pageBar.insertAdjacentHTML("beforeend", selectedPageBarForm(numberOfPage))
-      } else {
-        pageBar.insertAdjacentHTML("beforeend", pageBarForm(numberOfPage))
+
+    static showDetailArticle(articleId) {
+      window.location.href = BASE_URL + '/articles/' + articleId
+    }
+
+    static showArticleEditPage() {
+      window.location.href = BASE_URL + '/article/new'
+    }
+
+    renderPageBar(pageNumber, totalPages) {
+      const pageBar = document.getElementById('page-bar')
+
+      pageBar.innerHTML = ''
+
+      const startPageNumber = (pageNumber - 5) > 0 ? pageNumber - 5 : 1
+      if (startPageNumber !== 1) {
+        pageBar.insertAdjacentHTML("beforeend", ArticleTemplates.prevBarTemplate(startPageNumber - 1))
+      }
+      const lastPageNumber = (pageNumber + 5) <= totalPages ? pageNumber + 5 : totalPages
+      for (let numberOfPage = startPageNumber; numberOfPage <= lastPageNumber; numberOfPage++) {
+        if (numberOfPage === pageNumber) {
+          pageBar.insertAdjacentHTML("beforeend", ArticleTemplates.selectedPageBarTemplate(numberOfPage))
+        } else {
+          pageBar.insertAdjacentHTML("beforeend", ArticleTemplates.pageBarTemplate(numberOfPage))
+        }
+      }
+      if (lastPageNumber !== totalPages) {
+        pageBar.insertAdjacentHTML("beforeend", ArticleTemplates.nextBarTemplate(parseInt(lastPageNumber) + 1))
       }
     }
-    if (lastPageNumber !== totalPages) {
-      pageBar.insertAdjacentHTML("beforeend", nextBarForm)
+  }
+
+
+  class Controller {
+    constructor(articleService) {
+      this.articleService = articleService
+    }
+
+    showArticles(numberOfPage) {
+      this.articleService.showArticles(numberOfPage)
+    }
+
+    showDetailArticle(articleId) {
+      ArticleService.showDetailArticle(articleId)
+    }
+
+    showArticleEditPage() {
+      ArticleService.showArticleEditPage()
     }
   }
 
-  articleList.addEventListener('mouseover', function (e) {
-    const node = e.target.parentNode
-    if (node.className === 'article-info') {
-      this.style.cursor = 'pointer'
-    }
-  })
-
-  articleList.addEventListener('click', function (e) {
-    const node = e.target.parentNode
-    if (node.className === 'article-info') {
-      const articleId = node.querySelector('.article-info-article-id-content').getAttribute('value')
-      window.location.href = origin + '/articles/' + articleId
-    }
-  })
-
-
-  pageBar.addEventListener('click', function (e) {
-    const node = e.target
-    if (node.className === 'page-number') {
-      const pageNumber = node.innerText
-      getArticle(pageNumber)
-    }
-    if (node.className === 'next-bar') {
-      const pageNumbers = node.parentElement.getElementsByClassName('page-number')
-      const pageNumber = pageNumbers[pageNumbers.length - 1].innerText
-      console.log(pageNumber)
-      getArticle(parseInt(pageNumber) + 1)
-    }
-    if (node.className === 'prev-bar') {
-      const pageNumber = node.parentElement.getElementsByClassName('page-number')[0].innerText
-      getArticle(pageNumber - 1)
-    }
-  })
-
-  function getArticle(pageNumber) {
-    fetch(origin + "/api/articles?page=" + pageNumber, {
-      method: 'GET'
-    }).then(response => response.json())
-      .then(articleResponse => {
-        renderPageBar(articleResponse.pageNumber, articleResponse.totalPages)
-        articleList.innerHTML = ''
-        articleResponse.articles.forEach(article => {
-          articleList.insertAdjacentHTML("beforeend", articleListForm(article))
-        })
-      })
-      .catch(error => alert('오류가 발생했습니다.'));
-  }
-
-  articleCreateIcon.addEventListener('click', function (e) {
-    window.location.href = origin + '/article/new'
-  })
-
-}
-
-articleList()
+  return new Controller(new ArticleService())
+})()
