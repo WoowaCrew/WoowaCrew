6d5da645487814692cf43b86203488b84995d8e6
SunHo Van <vsh123@naver.com>
2019-12-12 14:49:02 +0900

    refactor: admin 페이지

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -23,16 +23,7 @@ const AdminApp = (() => {
     }
 
     async approveUser(userId) {
-      const userInfo = document.getElementById("user-" + userId)
-      const degreeDiv = userInfo.querySelector(".woowacrew.degree")
-      const roleDiv = userInfo.querySelector(".role")
-
-      const degree = degreeDiv.options[degreeDiv.selectedIndex].value
-      const role = roleDiv.options[roleDiv.selectedIndex].value
-
-      const data = new Object()
-      data.role = role
-      data.degreeNumber = degree
+      const data = this.getDegreeAndRole(userId)
 
       fetch(BASE_URL + "/api/users/" + userId + "/approve", {
         method: 'PUT',
@@ -46,22 +37,39 @@ const AdminApp = (() => {
       }).catch(error => alert("에러가 발생했습니다."))
     }
 
+    async updateUserAuthority(userId) {
+      const data = this.getDegreeAndRole(userId)
+      fetch(BASE_URL + "/api/users/" + userId + "/approve", {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => {
+        alert("정상적으로 승인 되었습니다.")
+      }).catch(error => alert("에러가 발생했습니다."))
+    }
+
+    getDegreeAndRole(userId) {
+      const userInfo = document.getElementById("user-" + userId)
+      const degreeDiv = userInfo.querySelector(".degree")
+      const roleDiv = userInfo.querySelector(".role")
+
+      const degree = degreeDiv.options[degreeDiv.selectedIndex].value
+      const role = roleDiv.options[roleDiv.selectedIndex].value
+
+      const data = new Object()
+      data.role = role
+      data.degreeNumber = degree
+
+      return data
+    }
+
     async showApprovedUser() {
-      const infoTitle = document.getElementById('info-title')
-      const infoContent = document.getElementById('info-content')
       fetch(origin + "/api/users/approve", {
         method: 'GET'
       }).then(response => response.json())
-        .then(users => {
-          infoTitle.innerHTML = ''
-          infoContent.innerHTML = ''
-          infoTitle.insertAdjacentHTML("afterbegin", AdminTemplates.userInfoTitle())
-          users.forEach(user => {
-            let template = AdminTemplates.approvedUserListTemplate(user)
-            let element = this.selectRole(template, user.userRole)
-            infoContent.insertAdjacentElement("beforeend", element)
-          })
-        })
+        .then(users => this.renderUserList(users))
         .catch(error => alert('오류가 발생했습니다.'));
     }
 
@@ -84,35 +92,48 @@ const AdminApp = (() => {
     }
 
     async showDetailUsersOfDegree(degreeId) {
-      const infoTitle = document.getElementById('info-title')
-      const infoContent = document.getElementById('info-content')
       fetch(BASE_URL + "/api/degrees/" + degreeId + "/users", {
         method: 'GET'
       }).then(response => response.json())
-        .then(users => {
-          infoTitle.innerHTML = ''
-          infoContent.innerHTML = ''
-          infoTitle.insertAdjacentHTML("afterbegin", AdminTemplates.userInfoTitle())
-          users.forEach(user => {
-            let template = AdminTemplates.approvedUserListTemplate(user);
-            let element = this.selectRole(template, user.userRole)
-            infoContent.insertAdjacentElement("beforeend", element)
-          })
-        })
+        .then(users => this.renderUserList(users))
         .catch(error => alert('오류가 발생했습니다.'));
     }
 
+    renderUserList(users) {
+      const infoTitle = document.getElementById('info-title')
+      const infoContent = document.getElementById('info-content')
+      infoTitle.innerHTML = ''
+      infoContent.innerHTML = ''
+      infoTitle.insertAdjacentHTML("afterbegin", AdminTemplates.userInfoTitle())
+      users.forEach(user => {
+        let template = AdminTemplates.approvedUserListTemplate(user)
+        let elementTemplate = document.createElement('div')
+        elementTemplate.innerHTML = template
+        this.selectRole(elementTemplate, user.userRole)
+        this.selectDegree(elementTemplate, user.degreeResponseDto.degreeNumber)
+        infoContent.insertAdjacentElement("beforeend", elementTemplate)
+      })
+    }
+
     selectRole(template, userRole) {
-      let element = document.createElement('div')
-      element.innerHTML = template;
-      const roleSelectedBox = element.querySelector('.role')
+      const roleSelectedBox = template.querySelector('.role')
       const options = roleSelectedBox.options
       for (const option of options) {
         if (option.value === userRole) {
           option.selected = true
         }
       }
-      return element
+      return template
+    }
+
+    selectDegree(template, degreeNumber) {
+      const degreeSelectedBox = template.querySelector('.degree')
+      const options = degreeSelectedBox.options
+      for (const option of options) {
+        if (parseInt(option.value) === degreeNumber) {
+          option.selected = true
+        }
+      }
     }
 
     activeButton(leftBar) {
@@ -138,6 +159,10 @@ const AdminApp = (() => {
 
     approveUser(userId) {
       this.adminService.approveUser(userId)
+    }
+
+    updateUserAuthority(userId) {
+      this.adminService.updateUserAuthority(userId)
     }
 
     showApprovedUser() {
