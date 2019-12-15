744b0535ba169a5b0586f64518be5593fca8f329
hyojaekim <hj00686@naver.com>
2019-12-15 17:33:26 +0900

    test: 제목으로 게시글 검색하는 테스트 추가

---
+++ src/test/java/woowacrew/search/service/ArticleSearchServiceTest.java
@@ -0,0 +1,57 @@
+package woowacrew.search.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import woowacrew.article.free.domain.Article;
+import woowacrew.article.free.dto.ArticleResponseDtos;
+import woowacrew.degree.domain.Degree;
+import woowacrew.user.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ArticleSearchServiceTest {
+
+    @Mock
+    private Pageable mockPageable;
+
+    @Mock
+    private ArticleSearchInternalService mockArticleSearchInternalService;
+
+    @InjectMocks
+    private ArticleSearchService articleSearchService;
+
+    @Test
+    void 정상적으로_제목으로_게시글을_검색한다() {
+        String title = "hello";
+        String content = "bonjour";
+        User user = new User("userId", new Degree());
+
+        List<Article> articles = new ArrayList<>();
+        articles.add(new Article(title, content, user));
+        articles.add(new Article(title, content, user));
+        articles.add(new Article(title, content, user));
+
+        Page<Article> articlePages = new PageImpl<>(articles);
+
+        when(mockArticleSearchInternalService.findAllByTitle(anyString(), any())).thenReturn(articlePages);
+
+        ArticleResponseDtos actual = articleSearchService.findAllByTitle("find", mockPageable);
+
+        assertThat(actual.getArticles().size()).isEqualTo(3);
+        assertThat(actual.getPageNumber()).isEqualTo(1);
+        assertThat(actual.getTotalPages()).isEqualTo(1);
+    }
+}
