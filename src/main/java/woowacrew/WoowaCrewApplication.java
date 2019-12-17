96b5402d77705513e0e737309f9eb1d1c2ea2d82
songwongeun <wongeunsong94@gmail.com>
2019-12-17 20:02:57 +0900

    docs: REST DOCS (#143)
    * feat: gradle 설정 추가
    
    * feat: api-guide.adoc 추가
    
    * refactor: JpaEnableConfig 분리
    
    * feat: ApiDocumentUtils 생성
    
    * refactor: docs 접근시 로그인 필요 없도록 수정
    
    * feat: ArticleApiController 문서화
    
    * feat: pathParameters 추가
    
    * feat: AdminDegreeApiController 문서화
    
    * feat: JsonConverter 추가
    
    * feat: AdminApiController 문서화
    
    * refactor: SecurityContextSupport 활용하여 UserContext 적용
    
    * refactor: api-guide.adoc 수정

--- src/main/java/woowacrew/WoowaCrewApplication.java
+++ src/main/java/woowacrew/WoowaCrewApplication.java
@@ -2,9 +2,7 @@ package woowacrew;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class WoowaCrewApplication {
     private static final String PROPERTIES = "spring.config.location=classpath:/github.yml";
