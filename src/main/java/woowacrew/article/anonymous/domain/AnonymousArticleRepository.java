cd8a4d5f7da8e718634448b4fece0bbd64c1eb82
yk1028 <wongeunsong94@gmail.com>
2019-12-18 13:23:53 +0900

    feat: AnonymousArticleRepository 구현

---
+++ src/main/java/woowacrew/article/anonymous/domain/AnonymousArticleRepository.java
@@ -0,0 +1,6 @@
+package woowacrew.article.anonymous.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnonymousArticleRepository extends JpaRepository<AnonymousArticle, Long> {
+}
