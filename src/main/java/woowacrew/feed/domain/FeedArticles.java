f794e271aeb6b205841f5cd154d3ffc705faeeb9
SunHo Van <vsh123@naver.com>
2019-12-18 10:55:19 +0900

    feat: FeedArticles 일급 컬렉션 생성

---
+++ src/main/java/woowacrew/feed/domain/FeedArticles.java
@@ -0,0 +1,23 @@
+package woowacrew.feed.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FeedArticles {
+    private final List<FeedArticle> feedArticles;
+
+    public FeedArticles(List<FeedArticle> feedArticles) {
+        this.feedArticles = feedArticles;
+    }
+
+    public List<FeedArticle> getNotDuplicatedFeedArticles(FeedArticles savedArticles) {
+        return feedArticles.stream()
+                .filter(savedArticles::isNotExistLink)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isNotExistLink(FeedArticle feedArticle) {
+        return feedArticles.stream()
+                .noneMatch(savedFeedArticle -> savedFeedArticle.isSameLink(feedArticle));
+    }
+}
