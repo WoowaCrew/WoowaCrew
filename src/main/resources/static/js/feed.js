ced3e3b69d14ddea44f01b8507ceee5851b54b9f
vsh123 <32254689+vsh123@users.noreply.github.com>
2019-12-18 14:13:25 +0900

    feat: RSS feed (#144)
    * setting: rome 모듈 추가
    
    * feat: FeedSource Entity
    
    * feat: FeedArticle Entity
    
    * feat: RssReader
    
    * feat: RssReader
    
    * feat: FeedInternalService.save()
    
    * feat: FeedInternalService.registerFeedSource
    
    * feat: FeedService.registerFeedSource()
    
    * rename: rss -> feed 패키지 리네임
    
    * feat: FeedApiController.registerFeedSource()
    
    * feat: FeedArticleRepository.findAll(Pageable)
    
    * feat: GET /api/feeds
    
    * style: 피드게시판
    
    * style: 관리자 rss 등록 기능
    
    * fix: 이미 존재하는 url은 등록하지 않는다
    
    * feat: Feed Update
    
    * refactor: FeedArticle FeedSource 매핑
    
    * rename: registerDto -> FeedSourceDto
    
    * refactor: FeedArticleResponseDto에 FeedSourceDto 추가
    
    * style: rss 등록 페이지
    
    * fix: FeedSource validation
    
    * fix: error 처리
    
    * style: 피드게시판
    
    * fix: 테스트 코드 수정
    
    * feat: FeedArticle column 옵션 설정
    
    * feat: FeedArticles 일급 컬렉션 생성
    
    * feat: FeedArticles getter 추가
    
    * refactor: RssReader 컴포넌트로 변경
    
    * feat: FeedArticleRepository.findByFeedSource()
    
    * refactor: RssReader.readData() 스태틱으로 선언
    
    * feat: FeedSource에서 FeedArticles를 리턴
    
    * rename: controller 메서드명 변경
    
    * refactor: FeedInternalService
    
    * refactor: 미사용 쿼리 메서드 삭제
    
    * fix: 오타 수정
    
    * fix: 일급컬렉션 get unmodifiableList

---
+++ src/main/resources/static/js/feed.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const FeedApp = (() => {
+  const BASE_URL = window.location.origin
+
+  class FeedService {
+    async showFeeds(numberOfPage) {
+      const articleList = document.getElementById('article-list')
+
+      fetch(BASE_URL + "/api/feeds?page=" + numberOfPage, {
+        method: 'GET'
+      }).then(response => response.json())
+        .then(feedResponse => {
+          this.renderPageBar(feedResponse.pageNumber, feedResponse.totalPages)
+          articleList.innerHTML = ''
+          feedResponse.articles.forEach(feed => {
+            let template = FeedTemplates.feedListTemplate(feed)
+            articleList.insertAdjacentHTML("beforeend", template)
+          })
+        })
+        .catch(error => alert('오류가 발생했습니다.'));
+    }
+
+    renderPageBar(pageNumber, totalPages) {
+      const pageBar = document.getElementById('page-bar')
+
+      pageBar.innerHTML = ''
+
+      const startPageNumber = (pageNumber - 5) > 0 ? pageNumber - 5 : 1
+      if (startPageNumber !== 1) {
+        pageBar.insertAdjacentHTML("beforeend", FeedTemplates.prevBarTemplate(startPageNumber - 1))
+      }
+      const lastPageNumber = (pageNumber + 5) <= totalPages ? pageNumber + 5 : totalPages
+      for (let numberOfPage = startPageNumber; numberOfPage <= lastPageNumber; numberOfPage++) {
+        if (numberOfPage === pageNumber) {
+          pageBar.insertAdjacentHTML("beforeend", FeedTemplates.selectedPageBarTemplate(numberOfPage))
+        } else {
+          pageBar.insertAdjacentHTML("beforeend", FeedTemplates.pageBarTemplate(numberOfPage))
+        }
+      }
+      if (lastPageNumber !== totalPages) {
+        pageBar.insertAdjacentHTML("beforeend", FeedTemplates.nextBarTemplate(parseInt(lastPageNumber) + 1))
+      }
+    }
+  }
+
+
+  class Controller {
+    constructor(feedService) {
+      this.feedService = feedService
+    }
+
+    showFeeds(numberOfPage) {
+      this.feedService.showFeeds(numberOfPage)
+    }
+  }
+
+  return new Controller(new FeedService())
+})()
