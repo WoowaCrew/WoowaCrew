3e2ee3037d3aca142ea5bd37f4f116e4159b96ec
pkch93 <star_final@naver.com>
2019-12-20 09:04:04 +0900

    fix: test/resources/data.sql

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -33,15 +33,15 @@ INSERT INTO user (id, oauth_id, nickname, role, degree_id) VALUES (6, '14','woow
 INSERT INTO user (id, oauth_id, nickname, role, degree_id) VALUES (7, '15','woowacrew','ROLE_CREW', 2);
 INSERT INTO user (id, oauth_id, nickname, role, degree_id) VALUES (8, '15','woowacrew','ROLE_PRECOURSE', 1);
 
-INSERT INTO feed_source(source_url,description) values ('https://vsh123.github.io/feed.xml', 'SHAKEVAN');
+INSERT INTO feed_source(id,source_url,description) values (1,'https://vsh123.github.io/feed.xml', 'SHAKEVAN');
 
-INSERT INTO feed_article(title,link,published_date,feed_source_id) values('title1','link1','2019-10-01T00:00:00',1);
-INSERT INTO feed_article(title,link,published_date,feed_source_id) values('title2','link2','2019-10-02T00:00:00',1);
-INSERT INTO feed_article(title,link,published_date,feed_source_id) values('title3','link3','2019-10-03T00:00:00',1);
-INSERT INTO feed_article(title,link,published_date,feed_source_id) values('title4','link4','2019-10-04T00:00:00',1);
-INSERT INTO feed_article(title,link,published_date,feed_source_id) values('title5','link5','2019-10-05T00:00:00',1);
-INSERT INTO feed_article(title,link,published_date,feed_source_id) values('title6','link6','2019-10-06T00:00:00',1);
+INSERT INTO feed_article(id,title,link,published_date,feed_source_id) values(1,'title1','link1','2019-10-01T00:00:00',1);
+INSERT INTO feed_article(id,title,link,published_date,feed_source_id) values(2,'title2','link2','2019-10-02T00:00:00',1);
+INSERT INTO feed_article(id,title,link,published_date,feed_source_id) values(3,'title3','link3','2019-10-03T00:00:00',1);
+INSERT INTO feed_article(id,title,link,published_date,feed_source_id) values(4,'title4','link4','2019-10-04T00:00:00',1);
+INSERT INTO feed_article(id,title,link,published_date,feed_source_id) values(5,'title5','link5','2019-10-05T00:00:00',1);
+INSERT INTO feed_article(id,title,link,published_date,feed_source_id) values(6,'title6','link6','2019-10-06T00:00:00',1);
 
 
-INSERT INTO crew_article(title,content,author) values('1기 게시글','1기 게시글',1);
-INSERT INTO crew_article(title,content,author) values('2기 게시글','2기 게시글',7);
+INSERT INTO crew_article(id,title,content,author) values(1,'1기 게시글','1기 게시글',1);
+INSERT INTO crew_article(id,title,content,author) values(2,'2기 게시글','2기 게시글',7);
