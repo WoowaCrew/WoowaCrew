aa3b8598b8d741d56aba6754551205b88ca93465
SunHo Van <vsh123@naver.com>
2019-12-30 17:03:06 +0900

    fix: 임시 권한 삭제

--- src/main/java/woowacrew/security/AbstractSecurityConfig.java
+++ src/main/java/woowacrew/security/AbstractSecurityConfig.java
@@ -55,7 +55,6 @@ public abstract class AbstractSecurityConfig extends WebSecurityConfigurerAdapte
         http
                 .authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                .anyRequest().permitAll()
                 .antMatchers("/", "/login", "/login/**", "/search", "/search/**", "/docs/**").permitAll()
                 .antMatchers("/accessdeny", "/users/form", "/users/update").authenticated()
                 .anyRequest().hasAnyRole(UserRole.ROLE_CREW.getRoleName(), UserRole.ROLE_COACH.getRoleName(), UserRole.ROLE_ADMIN.getRoleName())
