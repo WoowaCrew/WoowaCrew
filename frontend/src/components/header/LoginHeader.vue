8d697926073e9920cf7e064b0d67b54021db2e83
vsh123 <32254689+vsh123@users.noreply.github.com>
2020-01-05 01:45:09 +0900

    feat: 로그인 컴포넌트 (#217)
    * feat: 로그인 완료시 frontend의 url로 가게 설정
    
    * feat: 로그인 버튼을 누르면 로그인 요청을 보낸다
    
    * setting: axios 추가
    
    * setting: no-console disable
    
    * feat: 로그인 버튼 및 로그인 완료 시 닉네임 표시

---
+++ frontend/src/components/header/LoginHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Login v-if="isNotLogin"></Login>
+    <Profile v-else></Profile>
+  </div>
+</template>
+
+<script>
+import Login from "./login/Login";
+import Profile from "./login/Profile";
+export default {
+  components: {
+    Login,
+    Profile
+  },
+  created() {
+    this.$store.dispatch("checkLogin");
+  },
+  computed: {
+    isNotLogin() {
+      if (this.$store.state.userContext === null) {
+        return true;
+      }
+      return false;
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/header/login/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-btn small color="primary">{{ setNickname }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    setNickname() {
+      const nickname = this.$store.state.userContext.nickname;
+      if (nickname === null) {
+        return "닉네임 미설정";
+      }
+      return nickname;
+    }
+  }
+};
+</script>
+
+<style></style>
