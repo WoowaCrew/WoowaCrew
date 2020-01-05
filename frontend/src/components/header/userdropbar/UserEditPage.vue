593223192ec58346e7db9167626b8ab1b043538c
vsh123 <32254689+vsh123@users.noreply.github.com>
2020-01-06 02:19:54 +0900

    feat:  로그인 된 유저가 닉네임을 클릭하면 DropDown, 로그아웃 (#226)
    * feat: 유저 드롭 다운 버튼
    
    * feat: CustomLogoutSuccessHandler
    
    * feat: 로그아웃 테스트 코드 추가

---
+++ frontend/src/components/header/userdropbar/UserEditPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-list-item href="/user/edit">
+    <p class="body-2 white--text">
+      회원정보 수정
+    </p>
+  </v-list-item>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
