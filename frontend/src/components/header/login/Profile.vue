1b8f245f80d1fec8a2e1da6c64693176b09ddb5b
vsh123 <32254689+vsh123@users.noreply.github.com>
2020-01-06 02:19:54 +0900

    feat:  로그인 된 유저가 닉네임을 클릭하면 DropDown, 로그아웃 (#226)
    * feat: 유저 드롭 다운 버튼
    
    * feat: CustomLogoutSuccessHandler
    
    * feat: 로그아웃 테스트 코드 추가

--- frontend/src/components/header/login/Profile.vue
+++ frontend/src/components/header/login/Profile.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    <v-btn small color="primary">{{ setNickname }}</v-btn>
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" v-on="on">{{ setNickname }}</v-btn>
+      </template>
+      <v-list dense small color="accent">
+        <AdminPage v-if="isAdmin"></AdminPage>
+        <UserEditPage></UserEditPage>
+        <Logout></Logout>
+      </v-list>
+    </v-menu>
   </div>
 </template>
 
 <script>
+import AdminPage from "../userdropbar/AdminPage";
+import UserEditPage from "../userdropbar/UserEditPage";
+import Logout from "../userdropbar/Logout";
 export default {
+  components: {
+    AdminPage,
+    UserEditPage,
+    Logout
+  },
   computed: {
     setNickname() {
       const nickname = this.$store.state.userContext.nickname;
@@ -13,6 +30,12 @@ export default {
         return "닉네임 미설정";
       }
       return nickname;
+    },
+    isAdmin() {
+      if (this.$store.state.userContext.role === "ROLE_ADMIN") {
+        return true;
+      }
+      return false;
     }
   }
 };
---
+++ frontend/src/components/header/userdropbar/Logout.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-list-item @click="logout">
+    <p class="body-2 white--text">
+      로그아웃
+    </p>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+    }
+  }
+};
+</script>
+
+<style></style>
