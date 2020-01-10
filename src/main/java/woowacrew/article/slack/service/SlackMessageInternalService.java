a76298148ae3011cf702ad8fbae4e9ca038c2ab4
hyojaekim <hj00686@naver.com>
2020-01-10 11:05:16 +0900

    refactor: 예외 처리 수정
    - 예외에 대한 로그
    - session 닫아주기

--- src/main/java/woowacrew/article/slack/service/SlackMessageInternalService.java
+++ src/main/java/woowacrew/article/slack/service/SlackMessageInternalService.java
@@ -4,6 +4,8 @@ import com.ullink.slack.simpleslackapi.SlackChannel;
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.SlackUser;
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import woowacrew.article.slack.domain.SlackConfig;
@@ -13,9 +15,12 @@ import woowacrew.article.slack.dto.SlackMessageRequestDto;
 import woowacrew.article.slack.exception.CreateSlackMessageFailException;
 import woowacrew.article.slack.utils.SlackMessageConverter;
 
+import java.io.IOException;
+
 @Service
 @Transactional
 public class SlackMessageInternalService {
+    private static final Logger logger = LoggerFactory.getLogger(SlackMessageInternalService.class);
 
     private SlackConfig slackConfig;
     private SlackMessageRepository slackMessageRepository;
@@ -26,19 +31,21 @@ public class SlackMessageInternalService {
     }
 
     public SlackMessage save(SlackMessageRequestDto slackMessageRequestDto) {
-        SlackMessage slackMessage = createSlackMessage(slackMessageRequestDto);
-        return slackMessageRepository.save(slackMessage);
-    }
-
-    private SlackMessage createSlackMessage(SlackMessageRequestDto slackMessageRequestDto) {
         try {
-            SlackSession session = SlackSessionFactory.createWebSocketSlackSession(slackConfig.getToken());
-            session.connect();
-            SlackChannel channel = session.findChannelById(slackMessageRequestDto.getChannelId());
-            SlackUser author = session.findUserById(slackMessageRequestDto.getAuthorId());
-            return SlackMessageConverter.toEntity(channel.getName(), author.getUserName(), slackMessageRequestDto);
+            SlackMessage slackMessage = createSlackMessage(slackMessageRequestDto);
+            return slackMessageRepository.save(slackMessage);
         } catch (Exception e) {
+            logger.error("Create slack message fail : ", e);
             throw new CreateSlackMessageFailException();
         }
+    }
+
+    private SlackMessage createSlackMessage(SlackMessageRequestDto slackMessageRequestDto) throws IOException {
+        SlackSession session = SlackSessionFactory.createWebSocketSlackSession(slackConfig.getToken());
+        session.connect();
+        SlackChannel channel = session.findChannelById(slackMessageRequestDto.getChannelId());
+        SlackUser author = session.findUserById(slackMessageRequestDto.getAuthorId());
+        session.disconnect();
+        return SlackMessageConverter.toEntity(channel.getName(), author.getUserName(), slackMessageRequestDto);
     }
 }
