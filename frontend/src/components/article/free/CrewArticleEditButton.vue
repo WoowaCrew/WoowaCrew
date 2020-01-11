beef270f3f79b396f1e2396b6d06b7495b514896
SunHo Van <vsh123@naver.com>
2020-01-11 14:00:39 +0900

    feat: url path에 따라 저장 위치가 다르도록 변경

---
+++ frontend/src/components/article/free/CrewArticleEditButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-btn width="400" color="red" block @click="save">저장</v-btn>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: ["articleForm"],
+  methods: {
+    save() {
+      const title = this.articleForm.title;
+      const content = this.articleForm.content;
+      if (title.trim().length === 0) {
+        alert("제목을 입력해주세요");
+        return;
+      }
+      if (content.trim().length === 0) {
+        alert("내용을 입력해주세요");
+        return;
+      }
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("content", content);
+      axios("http://localhost:8080/api/articles/crew", {
+        method: "post",
+        data: formData,
+        withCredentials: true
+      }).then(res => {
+        if (res.status !== 201) {
+          alert("게시글 저장에 실패하였습니다.");
+          return;
+        }
+        const articleId = res.data.id;
+        alert("정상적으로 저장되었습니다");
+        window.location.href = "/articles/crew/" + articleId;
+      });
+    }
+  }
+};
+</script>
+
+<style></style>
