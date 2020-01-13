7bc87e7d3b1c4025f06e7e6cd6feaa8a7bf21196
vsh123 <32254689+vsh123@users.noreply.github.com>
2020-01-13 23:24:12 +0900

    feat: [Front]크루게시판, 자유게시판 상세 컴포넌트 작성 (#249)
    * feat: 자유게시판 데이터 불러오는 로직 분리
    
    * feat: crew게시판 상세 페이지 작성
    
    * refactor: 폴더명 변경
    
    * refactor: 경로 재설정
    
    /articles/:articleId -> /articles/free/:articleId

---
+++ frontend/src/components/article/view/CrewArticleView.vue
@@ -0,0 +1,33 @@
+<template> <div></div></template>
+<script>
+import axios from "axios";
+
+export default {
+  props: ["articleId"],
+  created() {
+    const articleId = this.articleId;
+    const requestUrl = "http://localhost:8080/api/articles/crew/" + articleId;
+
+    axios
+      .get(requestUrl, {
+        withCredentials: true
+      })
+      .then(res => {
+        const data = {
+          title: res.data.title,
+          content: res.data.content,
+          nickname: res.data.userResponseDto.nickname,
+          createdDate: res.data.createdDate
+        };
+        console.log(data);
+        this.$emit("setupData", data);
+        this.title = res.data.title;
+        this.content = res.data.content;
+        this.nickname = res.data.userResponseDto.nickname;
+        this.createdDate = res.data.createdDate;
+      });
+  }
+};
+</script>
+
+<style></style>
