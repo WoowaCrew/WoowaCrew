f499a4ebfb4eafaa5e82e66d040eac6915fbaaf9
hyojaekim <hj00686@naver.com>
2020-01-14 17:54:22 +0900

    feat: 게시물 제목 추가

--- frontend/src/components/side/menu/ArticleMenu.vue
+++ frontend/src/components/side/menu/ArticleMenu.vue
@@ -2,14 +2,19 @@
   <v-list>
     <v-list-item class="menu-header">
       <v-list-item-content>
-        <v-list-item-title>Communication</v-list-item-title>
+        <v-list-item-title>커뮤니케이션</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
 
     <v-list class="menu-buttons" dense nav>
       <v-list-item v-for="article in articles" :key="article.title" link>
         <v-list-item-content>
-          <v-list-item-title>{{ article.title }}</v-list-item-title>
+          <v-list-item-title style="color: #212121">
+            {{ article.title }}
+          </v-list-item-title>
+          <v-list-item-title>
+            {{ article.description }}
+          </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -24,10 +29,22 @@ export default {
   data() {
     return {
       articles: [
-        { title: "자유롭게 소통해요." },
-        { title: "같은 기수와 소통해요." },
-        { title: "비밀은 보장할게요. 단, 나쁜 말은 안 돼요!" },
-        { title: "어떤 블로그 게시을 볼까요?" }
+        {
+          title: "자유 게시판",
+          description: "자유롭게 소통해요."
+        },
+        {
+          title: "크루 게시판",
+          description: "같은 기수와 소통해요."
+        },
+        {
+          title: "익명 게시판",
+          description: "비밀은 보장할게요. 단, 나쁜 말은 안 돼요!"
+        },
+        {
+          title: "피드 게시판",
+          description: "어떤 블로그 게시물을 볼까요?"
+        }
       ]
     };
   }
