c1312ad8bc938442e22e5a85fb85212ab2b7edc9
Hyojae Kim <hj00686@naver.com>
2020-01-14 21:44:48 +0900

    feat: 왼쪽 메뉴바 추가 (#252)
    * feat: Side Navigation (왼쪽 메뉴) 추가
    
    - 게시글 버튼 추가
    
    * feat: 크루 서비스 버튼 추가
    
    * feat: 게시물 제목 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <v-app>
-    <v-app-bar app color="#78909C" dark>
+    <v-app-bar color="#78909C" dark app style="left: 0; z-index: 5">
       <div class="d-flex align-center">
         <span class="font-weight-black white--text">WoowaCrew</span>
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer />
 
-      <LoginHeader></LoginHeader>
+      <LoginHeader />
     </v-app-bar>
+
+    <SideNav />
 
     <v-content>
       <router-view />
@@ -18,12 +20,14 @@
 
 <script>
 import LoginHeader from "./components/header/LoginHeader";
+import SideNav from "./components/side/SideNav";
 
 export default {
   name: "App",
 
   components: {
-    LoginHeader
+    LoginHeader,
+    SideNav
   },
 
   data: () => ({
