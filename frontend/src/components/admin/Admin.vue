709329dcd3f4b712e2af8f6bbc4ee3f8ff5b2607
vsh123 <32254689+vsh123@users.noreply.github.com>
2020-01-15 21:44:10 +0900

    feat: [Front] 어드민 페이지/ 승인 대기 회원 (#256)
    * feat: put approve
    
    * feat: 유저 승인 페이지

---
+++ frontend/src/components/admin/Admin.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card style="height: 100%">
+    <v-layout row wrap fill-height>
+      <v-navigation-drawer permanent>
+        <v-list nav dense>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            @click="move(item.link)"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <router-view></router-view>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      item: 1,
+      items: [
+        { icon: "mdi-folder", title: "My Files", link: "a" },
+        {
+          icon: "mdi-account-multiple",
+          title: "승인 대기 회원 관리",
+          link: "/admin/user-approve"
+        },
+        { icon: "mdi-star", title: "Starred", link: "c" }
+      ]
+    };
+  },
+  methods: {
+    move(link) {
+      console.log(link);
+      window.location.href = link;
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/admin/UserApprove.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-simple-table class="mx-auto user-table" fixed-header height="600px">
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">아이디</th>
+          <th class="text-left">닉네임</th>
+          <th class="text-left">기수</th>
+          <th class="text-left">권한</th>
+          <th class="text-left">승인 버튼</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in userData" :key="item.id" :id="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.nickname }}</td>
+          <td>
+            <v-overflow-btn
+              v-model="form.degree[item.id]"
+              :items="degreeList"
+              label="기수를 선택해 주세요"
+            ></v-overflow-btn>
+          </td>
+          <td>
+            <v-overflow-btn
+              v-model="form.userRole[item.id]"
+              :items="userRoleList"
+              label="권한을 선택해 주세요"
+            ></v-overflow-btn>
+          </td>
+          <td>
+            <v-btn @click="approve(item.id, index)">
+              승인
+            </v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    approve(id, index) {
+      const degree = this.form.degree[id];
+      const userRole = this.form.userRole[id];
+      if (degree == null) {
+        alert("기수를 입력해주세요.");
+        return;
+      }
+      if (userRole == null) {
+        alert("권한을 입력해주세요.");
+        return;
+      }
+
+      const data = {
+        role: userRole,
+        degreeNumber: degree
+      };
+
+      axios("http://localhost:8080/api/users/2/approve", {
+        method: "put",
+        data: JSON.stringify(data),
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        withCredentials: true
+      }).then(res => {
+        res.data;
+        alert("정상적으로 저장되었습니다");
+        this.$delete(this.userData, index);
+      });
+    }
+  },
+  data() {
+    return {
+      form: {
+        degree: [],
+        userRole: []
+      },
+      degreeList: ["0", "1", "2"],
+      userRoleList: ["ROLE_PRECOURSE", "ROLE_CREW", "ROLE_COACH", "ROLE_ADMIN"],
+      userData: []
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8080/api/users/disapprove", {
+        withCredentials: true
+      })
+      .then(res => {
+        const object = res.data;
+        this.userData = object;
+      });
+  }
+};
+</script>
+
+<style>
+.user-table {
+  width: 800px;
+}
+</style>
