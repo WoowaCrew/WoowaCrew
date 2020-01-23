5b5388a26fc30483d07e178f30326aaeb795d615
vsh123 <32254689+vsh123@users.noreply.github.com>
2020-01-23 21:02:42 +0900

    feat: [Front] 어드민 페이지 - 크루 관리 (#267)
    * feat: degree 전체 리스트 불러오기
    
    * feat: 회원 권한 수정 기능

---
+++ frontend/src/components/admin/CrewDetail.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-card height="100%">
+    <v-simple-table class="mx-auto user-table" fixed-header height="600px">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">아이디</th>
+            <th class="text-left">닉네임</th>
+            <th class="text-left">기수</th>
+            <th class="text-left">권한</th>
+            <th class="text-left">수정 버튼</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in userData" :key="item.id" :id="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.nickname }}</td>
+            <td>{{ item.degreeResponseDto.degreeNumber }}</td>
+            <td>{{ item.userRole }}</td>
+            <td>
+              <CrewEditButton
+                :user="{
+                  id: item.id,
+                  degreeNumber: item.degreeResponseDto.degreeNumber,
+                  userRole: item.userRole
+                }"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+import CrewEditButton from "./crew/CrewEditButton";
+
+export default {
+  components: {
+    CrewEditButton
+  },
+  data() {
+    return {
+      degreeId: this.$route.query.degreeId,
+      userData: []
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8080/api/degrees/" + this.degreeId + "/users", {
+        withCredentials: true
+      })
+      .then(res => {
+        const object = res.data;
+        console.log(object);
+        this.userData = object;
+      });
+  }
+};
+</script>
+
+<style>
+.v-data-table {
+  width: 100%;
+}
+</style>
