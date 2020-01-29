344b6969fc362502c582042f90bca98432ad4172
vsh123 <32254689+vsh123@users.noreply.github.com>
2020-01-29 22:28:39 +0900

    feat: Rss 등록/관리 게시판 (#293)
    - 신규 등록 버튼위치 조정
    - 업데이트 버튼 생성

---
+++ frontend/src/components/admin/rss/RssUpdateButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-btn width="100" color="orange" @click="updateFeed">
+    업데이트
+  </v-btn>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    updateFeed() {
+      axios("http://localhost:8080/api/feeds/new", {
+        method: "post",
+        withCredentials: true
+      })
+        .then(res => {
+          if (res.status != 200) {
+            alert("오류가 발생했습니다. 다시한번 확인해주세요");
+            return;
+          }
+          alert("업데이트 완료!");
+        })
+        .catch(error => {
+          error.data;
+          alert("에러가 발생했습니다. 다시한번 확인해주세요");
+          return;
+        });
+    }
+  }
+};
+</script>
+
+<style></style>
