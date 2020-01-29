fa4890504dc0d6f15d5767e26133694749593264
Hyojae Kim <hj00686@naver.com>
2020-01-29 22:47:07 +0900

    feat: 검색창 추가 (#290)
    * feat: 검색창 추가 (프론트)
    
    * feat: (프론트) 검색 기능 추가
    
    * style: placeholder 수정

---
+++ frontend/src/components/home/SearchBox.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-text-field
+    shaped
+    dark
+    solo
+    outlined
+    autocomplete="off"
+    style="max-height: 56px"
+    background-color="#214b7d"
+    append-icon="fa-search search-icon"
+    placeholder="Google 검색하기.."
+    @input="setKeyword"
+    @click:append="search"
+    @keydown="search"
+  />
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "SearchBox",
+  data() {
+    return {
+      keyword: ""
+    };
+  },
+  methods: {
+    setKeyword(keyword) {
+      this.keyword = keyword;
+    },
+    search(key) {
+      if (key.code === "Enter" || key.type === "click") {
+        axios
+          .post("http://localhost:8080/api/search", {
+            content: this.keyword
+          })
+          .finally(() => {
+            window.open("https://www.google.com/search?q=" + this.keyword);
+          });
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.search-icon {
+  color: rgba(255, 255, 255, 0.4) !important;
+}
+
+.search-icon:hover {
+  cursor: pointer;
+}
+</style>
