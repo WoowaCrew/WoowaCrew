0d80ec8dd936bccc0030617952e311e80945b137
vsh123 <32254689+vsh123@users.noreply.github.com>
2020-02-15 18:07:15 +0900

    feat: 피드 업데이트를 연속적으로 요청할 수 없다. (#336)
    * feat: 피드 업데이트를 연속적으로 요청할 수 없다.
    
    * feat: 피드 업데이트 버튼 한번 클릭시 비활성화 처리

--- frontend/src/components/admin/rss/RssUpdateButton.vue
+++ frontend/src/components/admin/rss/RssUpdateButton.vue
@@ -1,7 +1,6 @@
 <template>
-  <v-btn text small color="orange" @click="updateFeed">
-    <v-icon>mdi-refresh</v-icon>
-    새로고침
+  <v-btn text small color="orange" :disabled="!isUpdatable" @click="updateFeed">
+    <v-icon>mdi-refresh</v-icon>새로고침
   </v-btn>
 </template>
 
@@ -9,27 +8,36 @@
 import axios from "axios";
 
 export default {
+  data() {
+    return {
+      isUpdatable: true
+    };
+  },
   methods: {
-    updateFeed() {
-      axios(this.$store.state.requestUrl + "/api/feeds/new", {
-        method: "post",
-        withCredentials: true
-      })
-        .then(res => {
-          if (res.status != 200) {
-            alert("오류가 발생했습니다. 다시한번 확인해주세요");
-            return;
-          }
-          alert("업데이트 완료!");
-        })
-        .catch(error => {
-          error.data;
-          alert("에러가 발생했습니다. 다시한번 확인해주세요");
-          return;
-        });
+    async updateFeed() {
+      if (!this.isUpdatable) {
+        alert("이미 업데이트 실행 중입니다.");
+        return;
+      }
+      this.isUpdatable = false;
+      const { status: status } = await axios(
+        this.$store.state.requestUrl + "/api/feeds/new",
+        {
+          method: "post",
+          withCredentials: true
+        }
+      ).finally(() => {
+        this.isUpdatable = true;
+      });
+      if (status === 200) {
+        alert("업데이트 완료!");
+        return;
+      }
+      alert("오류가 발생했습니다. 다시한번 확인해주세요");
     }
   }
 };
 </script>
 
-<style></style>
+<style>
+</style>
