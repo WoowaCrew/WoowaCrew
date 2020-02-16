1b431785d6b3db2f118df234087f3601e1536c2f
SunHo Van <vsh123@naver.com>
2020-02-16 15:08:07 +0900

    fix: 테스트 수정

--- src/test/java/woowacrew/article/slack/domain/SlackMessageRepositoryTest.java
+++ src/test/java/woowacrew/article/slack/domain/SlackMessageRepositoryTest.java
@@ -3,7 +3,6 @@ package woowacrew.article.slack.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,12 +12,11 @@ class SlackMessageRepositoryTest {
     private SlackMessageRepository slackMessageRepository;
 
     @Test
-    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     void 정상적으로_공지사항을_가져오는지_확인한다() {
         SlackMessage actualMessage =
-                slackMessageRepository.findFirstByChannelOrderByCreatedDateDesc("전체_공지사항")
+                slackMessageRepository.findFirstByChannelOrderByCreatedDateDesc("전체-공지사항")
                         .orElseThrow(IllegalArgumentException::new);
 
-        assertThat(actualMessage.getChannel()).isEqualTo("전체_공지사항");
+        assertThat(actualMessage.getChannel()).isEqualTo("전체-공지사항");
     }
 }
