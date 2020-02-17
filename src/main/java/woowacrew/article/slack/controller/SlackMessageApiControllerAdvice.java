b21a684a888fd857a7687b850d79e4f905b6f0a5
hyojaekim <hj00686@naver.com>
2020-02-17 11:44:25 +0900

    feat: CreateSlackMessageFailException 예외 처리
    - CreateSlackMessageFailException 예외가 발생 -> BadRequest

--- src/main/java/woowacrew/article/slack/controller/SlackMessageApiControllerAdvice.java
+++ src/main/java/woowacrew/article/slack/controller/SlackMessageApiControllerAdvice.java
@@ -3,6 +3,7 @@ package woowacrew.article.slack.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import woowacrew.article.slack.exception.CreateSlackMessageFailException;
 import woowacrew.article.slack.exception.NotFoundRecentlySlackMessageException;
 
 @ControllerAdvice
@@ -10,5 +11,10 @@ public class SlackMessageApiControllerAdvice {
     @ExceptionHandler(NotFoundRecentlySlackMessageException.class)
     public ResponseEntity<Void> notFound() {
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(CreateSlackMessageFailException.class)
+    public ResponseEntity<Void> createSlackMessageFail() {
+        return ResponseEntity.badRequest().build();
     }
 }
