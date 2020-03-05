55359cd082482c68f64a9c5d1022d28f837a3b0f
junk-sound <wom2277@naver.com>
2020-03-05 19:36:02 +0900

    feat: axiosMethod를 정의하고, 입력값에 따라 axiosMethod를 가져오는 함수(getMethod)를 만듦.

---
+++ frontend/src/api/httpClient/axios/axios.method.test.js
@@ -0,0 +1,16 @@
+import {getMethod, method} from "./axios.method";
+jest.mock('axios');
+let mockAxios = require('axios');
+
+describe('#getMethod()', () =>  {
+  test('should return axiosMethod', () => {
+    expect(getMethod(method.GET, mockAxios)).toEqual(mockAxios.get);
+    expect(getMethod(method.POST, mockAxios)).toEqual(mockAxios.post);
+    expect(getMethod(method.PUT, mockAxios)).toEqual(mockAxios.put);
+    expect(getMethod(method.DELETE, mockAxios)).toEqual(mockAxios.delete);
+  });
+
+  test('should throw exception', () => {
+    expect(() => getMethod("WrongMethod", mockAxios)).toThrowError(TypeError);
+  })
+});
