9e6b7079c524469032dce90a604982405bda72d1
hyojaekim <hj00686@naver.com>
2020-07-10 12:41:33 +0900

    feat: 커밋 정보 저장하는 스케줄러 추가

---
+++ woowacrew-web/src/main/java/woowacrew/github/service/GithubCommitSchedulerService.java
@@ -0,0 +1,24 @@
+package woowacrew.github.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import woowacrew.github.dto.GithubCommitRequestDto;
+
+import java.time.LocalDate;
+
+@Service
+public class GithubCommitSchedulerService {
+
+    private final GithubCommitService githubCommitService;
+
+    public GithubCommitSchedulerService(GithubCommitService githubCommitService) {
+        this.githubCommitService = githubCommitService;
+    }
+
+    @Scheduled(cron = "0 0 9 * * *")
+    public void save() {
+        LocalDate now = LocalDate.now();
+        GithubCommitRequestDto requestDto = new GithubCommitRequestDto(now.getYear(), now.getMonthValue());
+        this.githubCommitService.save(requestDto);
+    }
+}
